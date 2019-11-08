0dbb6670d45f45235a426064ae26385d7a0013c1
melvinkcx <melvinkcx@gmail.com>
2019-11-08 15:59:29 +0900

    Cleaned up leftover commands and files

--- freeze.sh
+++ freeze.sh
@@ -2,6 +2,5 @@ pyinstaller --onefile \
   --windowed \
   --hidden-import configparser \
   --add-data './view/dist:./view/dist' \
-  --add-data './config.template.yml:./config.yml' \
   --icon icon.ico \
   main.py
