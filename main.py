c8359444c9d24948e46166f347c9772c8a2d5b33
melvinkcx <melvinkcx@gmail.com>
2019-11-08 09:16:17 +0900

    Configured view to build properly

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from api import JsApi
 
 if __name__ == "__main__":
     api = JsApi()
-    window = webview.create_window("Private File Bucket", url="http://localhost:8080/index.html", js_api=api, width=660,
+    window = webview.create_window("Private File Bucket", url="view/dist/index.html", js_api=api, width=660,
                                    height=800)
-    webview.start(debug=True)
+    webview.start(debug=False)
